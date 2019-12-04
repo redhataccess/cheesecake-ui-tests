58b98336b7c6ce89763779b2883f30e7c130b358
nmath <nmath@localhost.localdomain>
2019-12-04 16:07:37 +0530

    Added edit metadata test

---
+++ pages/display_module_page.py
@@ -0,0 +1,12 @@
+import helpers.base as base
+from helpers import locators
+import sys
+from helpers import utilities
+sys.path.append("..")
+
+#Reset edit metadata form
+def reset_edit_metadata_from(driver):
+    utilities.select_value_from_dropdown(driver, locators.PRODUCT_NAME_DROPDOWN_CSS, "Select a Product")
+    utilities.select_value_from_dropdown(driver, locators.PRODUCT_VERSION_DROPDOWN_CSS, "Select a Version")
+    utilities.select_value_from_dropdown(driver, locators.PRODUCT_USECASE_DROPDOWN_CSS, "Select Use Case")
+    utilities.enter_text_by_css_selector(driver, locators.PRODUCT_URLFRAGMENT_CSS, "")
